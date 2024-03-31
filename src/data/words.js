c757905bd25c518922fb82bf6c27a925d38f62b3
Victor Hugo <vhsds2000@gmail.com>
2024-03-31 11:36:40 -0400

    first commit

---
+++ src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capo", "Pneu", "Antena", "Farol", "Calota", "Volante"],
+    fruta: ["Banana", "Maçã", "Pêra", "Mamão", "Laranja", "Cupuaçu", "Graviola", "Maracujá"],
+    corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Barriga", "Panturrilha", "Nadegas"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete", "Webcam", "Sistema", "Driver"],
+    programação: ["Linguagem", "Framework", "JavaScript", "React", "TypeScript", "MongoDB", "Angular"],
+    alimento: ["Arroz", "Feijão", "Carne", "Leite", "Farinha", "Tapioca", "Tomate", "Batata"],
+  };
